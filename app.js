9cde8c930083e4cb23152a4857e35abacbccbd16
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2017-12-23 05:04:56 +0100

    Added image file size

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-var path = require('path')
- 
-var argv = require('yargs').argv;
+const path = require('path');
 
-var pathToRead = argv.path;
-var fs = require('fs');
+const argv = require('yargs').argv;
 
-var regexps = require('./regexps');
-var imageTest = regexps.imageTest;
+let pathToRead = argv.path;
+const fs = require('fs');
+
+const regexps = require('./regexps');
+const imageTest = regexps.imageTest;
 
 if (path.isAbsolute(pathToRead)) {
   pathToRead = path.relative(__dirname, pathToRead) || './';
 };
 
-var readAbsolutePath = function readAbsolutePath (pathToRead) {
+const readAbsolutePath = function readAbsolutePath (pathToRead) {
   fs.readdir(pathToRead, function (err, files) {
     if (err) {
-      throw err;
+      throw new Error(err);
     }
 
     files.forEach(function (file) {
-      var absoluteFilePath = path.join(__dirname, pathToRead, file);
+      const absoluteFilePath = path.join(__dirname, pathToRead, file);
       if (imageTest(file)) {
-        console.log(absoluteFilePath);
+        const stats = fs.statSync(absoluteFilePath);
+        console.log(`${absoluteFilePath} : ${stats['size']/1000.0}KB`);
       } else if (fs.lstatSync(absoluteFilePath).isDirectory()) {
         readAbsolutePath(path.join(pathToRead, file));
       }
