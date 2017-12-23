416fbe4e919376a72a88625c476dbebabae2d75d
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2017-12-23 04:21:39 +0100

    Added primitive recursive search

--- app.js
+++ app.js
@@ -2,20 +2,31 @@ var path = require('path')
  
 var argv = require('yargs').argv;
 
-var testFolder = argv.path;
+var pathToRead = argv.path;
 var fs = require('fs');
 
 var regexps = require('./regexps');
 var imageTest = regexps.imageTest;
 
-fs.readdir(testFolder, function (err, files) {
-  if (err) {
-    throw err;
-  }
-  
-  files.forEach(function (file) {
-    if(imageTest(file)){
-      console.log(path.join(__dirname, testFolder, file))
+
+var readAbsolutePath = function readAbsolutePath (pathToRead) {
+  fs.readdir(pathToRead, function (err, files) {
+    if (err) {
+      throw err;
     }
-  });
-})
+
+    files.forEach(function (file) {
+      var absoluteFilePath = path.join(__dirname, pathToRead, file);
+
+      if (imageTest(file)) {
+        console.log(absoluteFilePath);
+      } else if (fs.lstatSync(absoluteFilePath).isDirectory()) {
+        readAbsolutePath(path.join(pathToRead, file));
+      }
+    });
+  })
+};
+
+readAbsolutePath(pathToRead);
+
+
